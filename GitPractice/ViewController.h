cd1da02040d4067ac0d019317cbaabdef3542e77
irwingonzales92 <irwin@newdesto.com>
2015-01-27 14:19:12 -0800

    Initial Commit

---
+++ GitPractice/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GitPractice
+//
+//  Created by Irwin Gonzales on 1/27/15.
+//  Copyright (c) 2015 Irwin Gonzales. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
